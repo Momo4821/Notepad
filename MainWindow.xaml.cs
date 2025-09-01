aa8adc87313b3defc42574e5e99eccbab52711a1
Malania <wilsonmcw4@gmail.com>
2025-09-01 18:08:46 -0400

    Implemented MenuItem Buttons and Open File Function.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,39 @@ namespace Notepad
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void OpenFile_OnClick(object sender, RoutedEventArgs e)
+        {
+
+
+
+            //open file
+            var file_Content = string.Empty; // will hold the content of the file
+            var file_Path = string.Empty; // will hold the path of the file
+
+            using OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file_Path = openFileDialog.FileName;
+                var file_Stream = openFileDialog.OpenFile();
+
+
+                using (StreamReader reader = new StreamReader(file_Stream))
+                {
+                    file_Content = reader.ReadToEnd();
+
+                }
+
+            }
+
+
+           
+
         }
     }
 }
