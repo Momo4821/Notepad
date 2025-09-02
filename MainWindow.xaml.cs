4deb592d8527f97b30839813b335d7c969832ea1
Malania <wilsonmcw4@gmail.com>
2025-09-01 22:24:43 -0400

    Created empty methods for other on click actions.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using MessageBox = System.Windows.MessageBox;
+using Path = System.IO.Path;
 using TextBox = System.Windows.Controls.TextBox;
 
 namespace Notepad
@@ -32,25 +34,35 @@ namespace Notepad
             var file_Content = string.Empty; 
             var file_Path = string.Empty; 
             using OpenFileDialog openFileDialog = new OpenFileDialog();
+            var file_type = string.Empty;
+            
+
+            
+
 
             openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
 
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                file_Path = openFileDialog.FileName; 
+        
+                file_Path = openFileDialog.FileName;
+                file_type = openFileDialog.DefaultExt;
                 var file_Stream = openFileDialog.OpenFile();
-
+               
 
                 try
                 {
-
+                   
                     using (StreamReader reader = new StreamReader(file_Stream))
                     {
                         Texbox_Main.Text = reader.ReadToEnd(); //display file in text
                         Textblock_File_Path.Text = file_Path; //display file path
+                        Textblock_File_Type.Text = file_type;
+
 
                     }
-                   
+
+                    
 
 
                 }
@@ -81,10 +93,48 @@ namespace Notepad
         }
 
 
+        public void Save_OnClick(object sender, RoutedEventArgs e)
+        {
+            
 
-      
+            //if textbox is modified give the option to save
+            MessageBoxButtons Buttons = MessageBoxButtons.YesNoCancel;
+            string caption = "Do you wish to save your progress";
+            DialogResult result;
+
+                //messagebox to show if the user should save or not
+
+                using (StreamWriter writer = new StreamWriter())
 
 
+        }
+           
+
+
+        private void Save_As_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Print_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Time_Date_Stamp_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Change_Font_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
