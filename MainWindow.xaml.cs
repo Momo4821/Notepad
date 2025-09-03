aadb1b4d50859de17d6caa182f2b46eec1f7f30f
Malania <wilsonmcw4@gmail.com>
2025-09-02 23:57:59 -0400

    Progress on Save Button.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using System.Windows.Media.Animation;
 using MessageBox = System.Windows.MessageBox;
 using Path = System.IO.Path;
 using TextBox = System.Windows.Controls.TextBox;
@@ -26,6 +27,10 @@ namespace Notepad
         {
             InitializeComponent();
         }
+
+
+        
+
 
 
         public void OpenFile_OnClick(object sender, RoutedEventArgs e)
@@ -68,21 +73,9 @@ namespace Notepad
                 }
                 catch (Exception exception)
                 {
-                    Console.WriteLine("Invalid File");
+                    MessageBox.Show("Invalid File. Only txt");
                     throw;
                 }
-
-            
-
-       
-            
-
-
-
-                    //
-                ; //display file in text
-
-
 
             }
 
@@ -95,17 +88,31 @@ namespace Notepad
 
         public void Save_OnClick(object sender, RoutedEventArgs e)
         {
-            
+       
 
             //if textbox is modified give the option to save
             MessageBoxButtons Buttons = MessageBoxButtons.YesNoCancel;
             string caption = "Do you wish to save your progress";
             DialogResult result;
 
+          
+
+
+            result = System.Windows.Forms.MessageBox.Show(caption,Textblock_File_Path.Text, Buttons);
+
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                //we need to append the file that is open in the text box
+                using (StreamWriter sw = File.AppendText(Textblock_File_Path.Text.ToString()))
+                {
+                    sw.WriteLine(Texbox_Main.Text);
+                    
+                    
+                }
+            }
+
             //messagebox to show if the user should save or not
-
-           
-
 
         }
            
@@ -113,7 +120,14 @@ namespace Notepad
 
         private void Save_As_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+
+
+            DirectoryInfo[] currentdirectory = new DirectoryInfo("C:").GetDirectories();
+
+
+
+
+
         }
 
         private void Print_OnClick(object sender, RoutedEventArgs e)
@@ -123,7 +137,12 @@ namespace Notepad
 
         private void Exit_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            string Exit_Caption = "Are you sure you want to exit?";
+
+            MessageBoxResult result = MessageBox.Show(Exit_Caption, "Exit Notepad",
+                MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            this.Close();
         }
 
         private void Time_Date_Stamp_OnClick(object sender, RoutedEventArgs e)
