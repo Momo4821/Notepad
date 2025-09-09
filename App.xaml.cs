27f5119f62efeaa113d00f1c36ef1a3edb406c56
Malania <wilsonmcw4@gmail.com>
2025-09-08 21:16:25 -0400

    Fixed SaveonClick Meothod. Need to still implement a check if file/path exists.

--- App.xaml.cs
+++ App.xaml.cs
@@ -10,6 +10,18 @@ namespace Notepad
     /// </summary>
     public partial class App : Application
     {
+        private void Window_Startup(object sender, StartupEventArgs args)
+        {
+            Window window1 = new Window();
+
+            window1.Title = "Notepad";
+            window1.Height = 600;
+            window1.Width = 800;
+            window1.Show();
+        }
+        
+
+
     }
 
-}
+}                  
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,37 +30,38 @@ namespace Notepad
             InitializeComponent();
         }
 
+
+
+
+
+        public OpenFileDialog openfile;
+        public string file_Path { get; set; }
+
         public void OpenFile_OnClick(object sender, RoutedEventArgs e)
         {
-           var file_Path = string.Empty; 
             using OpenFileDialog openFileDialog = new OpenFileDialog();
             var file_type = string.Empty;
 
             openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-        
+
                 file_Path = openFileDialog.FileName;
                 file_type = Path.GetExtension(file_Path);
                 var file_Stream = openFileDialog.OpenFile();
-               
+
 
                 try
                 {
-                   
+
                     using (StreamReader reader = new StreamReader(file_Stream))
                     {
-                        Texbox_Main.Text = reader.ReadToEnd(); //display file in text
+                        Textbox_Main.Text = reader.ReadToEnd(); //display file in text
                         Textblock_File_Path.Text = file_Path; //display file path
                         Textblock_File_Type.Text = file_type;
 
 
                     }
-
-                    
-
-
                 }
                 catch (Exception exception)
                 {
@@ -68,121 +69,103 @@ namespace Notepad
                     throw;
                 }
 
-
-
-
-
             }
         }
 
 
+
         public void Save_OnClick(object sender, RoutedEventArgs e)
+
         {
-       
-
-            //if textbox is modified give the option to save
+            //Messagebox for save file
             MessageBoxButtons Buttons = MessageBoxButtons.YesNoCancel;
-            string caption = "Do you wish to save your progress";
-            DialogResult result;
+            string caption = "Do you wish to save file?";
+            DialogResult Result;
+            Result = System.Windows.Forms.MessageBox.Show(caption, String.Empty, Buttons);
+            Convert.ToBoolean(Result);
 
-          
+            
+            //save dioluge option                                   
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            
+            
+            //file path can't be null so we need to get directories for the file to be saved
+            //DirectoryInfo directory = new DirectoryInfo(Path.GetDirectoryName(@"C:\Downloads"));
 
-
-            result = System.Windows.Forms.MessageBox.Show(caption,Textblock_File_Path.Text, Buttons);
-
-
-            if (result == System.Windows.Forms.DialogResult.Yes)
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            saveFileDialog.AddExtension = true;
+            
+            switch (Result)
             {
-                //we need to append the file that is open in the text box
-            /*    using (StreamWriter sw = File.AppendText(Textblock_File_Path.Text.ToString()))
-                {
-                    sw.WriteLine(Texbox_Main.Text); 
-                    
-                }*/
-            File.AppendText(Textblock_File_Path.ToString());
-            File.WriteAllText(Textblock_File_Path.Text.ToString(),Texbox_Main.Text);
+                case System.Windows.Forms.DialogResult.Yes:
+                    if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        
+                        using StreamWriter sw = new StreamWriter(saveFileDialog.FileName);
+                        {
+                            sw.Write(Textbox_Main.Text.ToString());
+                            
+                        }
+                        
+                        
+                    }
 
+                    break;    
+                
+                case System.Windows.Forms.DialogResult.No:
+                    //
+
+                    break;
+                
+                case System.Windows.Forms.DialogResult.Cancel:
+                break;
             }
-
-            //messagebox to show if the user should save or not
-
-
-
-
-
-
-
-
-
-
-
-
+            
+            
 
 
 
 
         }
         
+        
+
         private void Save_As_OnClick(object sender, RoutedEventArgs e)
         {
 
 
             //this will save the file as a new file
-            SaveFileDialog save_as = new SaveFileDialog();
-
-            save_as.FileName = "Document"; // Default file name
-
-
-
+            if (file_Path != string.Empty)
+            {
+                
+                
+                
+            }
 
 
         }
 
         private void Print_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //THIS WILL PRINT THE OPEN DOCUMENT TO  A CONNECTED PRINTER
+
+
+
+
         }
 
         private void Exit_OnClick(object sender, RoutedEventArgs e)
         {
-            
-            
-            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
-            string caption = "Do you wish to save your progress";
-            DialogResult result;
-            result = System.Windows.Forms.MessageBox.Show(caption, Texbox_Main.Text, buttons);
-            Convert.ToBoolean(result);
-
-            switch (result)
-            {
-                case System.Windows.Forms.DialogResult.Yes :
-                    using StreamWriter sr = new StreamWriter;
-                    break;
-
-            }
-
-
-
-            //close app
             this.Close();
-
-
-
         }
 
         private void Time_Date_Stamp_OnClick(object sender, RoutedEventArgs e)
         {
-            DateTime date_stamp = DateTime.UtcNow;
-
-            Texbox_Main.Text = date_stamp.ToString();
 
 
-            if (Keyboard.IsKeyDown(Key.F5)) // need to work on 
-            {
-                
-                Texbox_Main.Text = date_stamp.ToString();
 
-            }
+
+
 
         }
 
@@ -198,13 +181,51 @@ namespace Notepad
 
         private void New_OnClick(object sender, RoutedEventArgs e)
         {
-            
+
+            MessageBoxButtons Buttons = MessageBoxButtons.YesNoCancel;
+            string caption = "Do you wish to save your file before creating a new one";
+            var new_file = new OpenFileDialog();
+            DialogResult result_new;
 
 
+
+            /*//create a new document
+            if (Textbox_Main.Text != string.Empty)
+            {
+                //result = System.Windows.Forms.MessageBox.Show(caption,Textblock_File_Path.Text, Buttons);
+                result_new = System.Windows.Forms.MessageBox.Show(caption, Textblock_File_Path.Text, Buttons);
+
+                if (result_new == System.Windows.Forms.DialogResult.Yes)
+                {
+                    File.AppendText(Textblock_File_Path.ToString());
+                }
+
+                Textbox_Main.Clear();
+                Textblock_File_Type.Text = string.Empty;
+                Textblock_File_Path.Text = string.Empty;
+
+
+
+            }*/
+
+
+
+
+
+
+
+
+
+
+            //when pressed new check if textbox has been modified give user option to save
+
+
+
+
+            //after save clear textbox
 
 
 
         }
     }
-
 }
